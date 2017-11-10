adbe0a43710e95753f03c1c8d272567d08891986
Peter Jang <peter.x.jang@gmail.com>
2017-11-09 20:31:46 -0600

    Add loop to keep asking for words

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -1,38 +1,47 @@
 require "unirest"
 
-system "clear"
-puts "Welcome to my Dictionary App!"
-print "Enter a word: "
-input_word = gets.chomp
+while true
+  system "clear"
+  puts "Welcome to my Dictionary App!"
+  print "Enter a word: "
+  input_word = gets.chomp
 
-response = Unirest.get("http://api.wordnik.com:80/v4/word.json/#{input_word}/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
-definitions = response.body
+  response = Unirest.get("http://api.wordnik.com:80/v4/word.json/#{input_word}/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
+  definitions = response.body
 
-response = Unirest.get("http://api.wordnik.com:80/v4/word.json/#{input_word}/topExample?useCanonical=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
-top_example = response.body
+  response = Unirest.get("http://api.wordnik.com:80/v4/word.json/#{input_word}/topExample?useCanonical=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
+  top_example = response.body
 
-response = Unirest.get("http://api.wordnik.com:80/v4/word.json/#{input_word}/pronunciations?useCanonical=false&limit=50&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
-pronunciations = response.body
+  response = Unirest.get("http://api.wordnik.com:80/v4/word.json/#{input_word}/pronunciations?useCanonical=false&limit=50&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
+  pronunciations = response.body
 
-system "say #{input_word}"
+  system "say #{input_word}"
 
-puts "DEFINITIONS"
-index = 0
-definitions.length.times do
-  puts "#{index}. #{definitions[index]["text"]}"
+  puts "DEFINITIONS"
+  index = 0
+  definitions.length.times do
+    puts "#{index}. #{definitions[index]["text"]}"
+    puts
+    index = index + 1
+  end
+
+  puts "TOP EXAMPLE"
+  puts top_example["text"]
   puts
-  index = index + 1
-end
 
-puts "TOP EXAMPLE"
-puts top_example["text"]
-puts
+  system "say #{top_example["text"]}"
 
-system "say #{top_example["text"]}"
+  puts "PRONUNCIATIONS"
+  index = 0
+  pronunciations.length.times do
+    puts "#{index}. #{pronunciations[index]["raw"]}"
+    index = index + 1
+  end
 
-puts "PRONUNCIATIONS"
-index = 0
-pronunciations.length.times do
-  puts "#{index}. #{pronunciations[index]["raw"]}"
-  index = index + 1
+  puts "Enter q to quit, any other key to continue"
+  input_option = gets.chomp
+  if input_option == "q"
+    puts "Thank you, goodbye!"
+    break
+  end
 end
